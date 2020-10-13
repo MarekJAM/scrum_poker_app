3311517d2c8a3d0c99008a161022e8aeafa706fd
Marek <marekash@gmail.com>
2020-10-13 23:56:38 +0200

    added password field

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -21,6 +21,7 @@ class _LoginScreenState extends State<LoginScreen> {
     text: "192.168.0.14:8080",
   );
   TextEditingController _userController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +33,7 @@ class _LoginScreenState extends State<LoginScreen> {
               decoration: BoxDecoration(
                 gradient: LinearGradient(
                   colors: [
-                    Color.fromRGBO(255, 127, 80, 1).withOpacity(0.5),
+                    Color.fromRGBO(255, 127, 80, 1).withOpacity(0.4),
                     Colors.blue,
                   ],
                   begin: Alignment.topLeft,
@@ -61,75 +62,98 @@ class _LoginScreenState extends State<LoginScreen> {
                       ),
                     ),
                     Center(
-                      child: Card(
-                        margin: EdgeInsets.all(30),
-                        child: Padding(
-                          padding: EdgeInsets.all(16),
-                          child: Form(
-                            key: _formKey,
-                            child: BlocConsumer<LoginBloc, LoginState>(
-                              listener: (ctx, state) {
-                                if (state is LoginConnectionError) {
-                                  CommonWidgets.displaySnackBar(
-                                    context: ctx,
-                                    message: state.message,
-                                    color: Theme.of(ctx).errorColor,
-                                  );
-                                }
-                              },
-                              builder: (context, state) {
-                                if (state is LoginDisconnectedFromServer) {
-                                  _userController.text = state.username;
-                                  _serverController.text = state.serverAddress;
-                                }
-                                return Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    TextFormField(
-                                      key: Key(Keys.inputServerAddress),
-                                      keyboardType: TextInputType.emailAddress,
-                                      decoration: InputDecoration(
-                                          labelText: 'Server address',
-                                          prefixIcon: Icon(Icons.dns)),
-                                      readOnly: state is LoginConnectingToServer
-                                          ? true
-                                          : false,
-                                      controller: _serverController,
-                                      validator: (value) {
-                                        if (value.isEmpty) {
-                                          return "Provide server address.";
-                                        }
-                                        return null;
-                                      },
+                      child: Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Form(
+                          key: _formKey,
+                          child: BlocConsumer<LoginBloc, LoginState>(
+                            listener: (ctx, state) {
+                              if (state is LoginConnectionError) {
+                                CommonWidgets.displaySnackBar(
+                                  context: ctx,
+                                  message: state.message,
+                                  color: Theme.of(ctx).errorColor,
+                                );
+                              }
+                            },
+                            builder: (context, state) {
+                              if (state is LoginDisconnectedFromServer) {
+                                _userController.text = state.username;
+                                _serverController.text = state.serverAddress;
+                              }
+                              return Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  TextFormField(
+                                    key: Key(Keys.inputServerAddress),
+                                    keyboardType: TextInputType.emailAddress,
+                                    decoration: InputDecoration(
+                                      labelText: 'Server address',
+                                      prefixIcon: Icon(Icons.dns),
+                                      
                                     ),
-                                    TextFormField(
-                                      key: Key(Keys.inputUsername),
-                                      decoration: InputDecoration(
-                                        labelText: 'Username',
-                                        prefixIcon: Icon(Icons.person),
-                                      ),
-                                      readOnly: state is LoginConnectingToServer
-                                          ? true
-                                          : false,
-                                      controller: _userController,
-                                      validator: (value) {
-                                        if (value.isEmpty) {
-                                          return "Provide username.";
-                                        } else if (value.trim().length > 20) {
-                                          return "Name too long - max. 20 characters.";
-                                        }
-                                        return null;
-                                      },
+                                    readOnly: state is LoginConnectingToServer
+                                        ? true
+                                        : false,
+                                    controller: _serverController,
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return "Provide server address.";
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  TextFormField(
+                                    key: Key(Keys.inputUsername),
+                                    decoration: InputDecoration(
+                                      labelText: 'Username',
+                                      prefixIcon: Icon(Icons.person),
                                     ),
-                                    SizedBox(
-                                      height: 20,
+                                    readOnly: state is LoginConnectingToServer
+                                        ? true
+                                        : false,
+                                    controller: _userController,
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return "Provide username.";
+                                      } else if (value.trim().length > 20) {
+                                        return "Name too long - max. 20 characters.";
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  TextFormField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Password',
+                                      prefixIcon: Icon(Icons.lock),
                                     ),
-                                    state is LoginConnectingToServer
-                                        ? CircularProgressIndicator()
-                                        : RaisedButton(
+                                    obscureText: true,
+                                    controller: _passwordController,
+                                    readOnly: state is LoginConnectingToServer
+                                        ? true
+                                        : false,
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return "Provide password.";
+                                      } else if (value.trim().length > 20) {
+                                        return "Password too long - max. 20 characters.";
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  SizedBox(
+                                    height: 20,
+                                  ),
+                                  state is LoginConnectingToServer
+                                      ? CircularProgressIndicator()
+                                      : ConstrainedBox(
+                                          constraints: const BoxConstraints(
+                                            minWidth: double.infinity,
+                                          ),
+                                          child: RaisedButton(
                                             key: Key(Keys.buttonConnect),
                                             child: Text(
-                                              'Connect',
+                                              'Login',
                                               style: TextStyle(
                                                 color: Theme.of(context)
                                                     .canvasColor,
@@ -137,21 +161,31 @@ class _LoginScreenState extends State<LoginScreen> {
                                             ),
                                             color:
                                                 Theme.of(context).primaryColor,
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(20),
-                                            ),
                                             onPressed: () {
                                               if (_formKey.currentState
                                                   .validate()) {
                                                 _onFormSubmitted();
                                               }
                                             },
-                                          )
-                                  ],
-                                );
-                              },
-                            ),
+                                          ),
+                                        ),
+                                  Divider(),
+                                  ConstrainedBox(
+                                    constraints: const BoxConstraints(
+                                      minWidth: double.infinity,
+                                    ),
+                                    child: RaisedButton(
+                                      onPressed: () {},
+                                      child: Text('Signup'),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text('Login as guest'),
+                                  ),
+                                ],
+                              );
+                            },
                           ),
                         ),
                       ),
