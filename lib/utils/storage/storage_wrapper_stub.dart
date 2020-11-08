68b25227c00ad662e9453c614417cbb18f297155
Marek <marekash@gmail.com>
2020-11-08 23:18:13 +0100

    changed print message

--- lib/utils/storage/storage_wrapper_stub.dart
+++ lib/utils/storage/storage_wrapper_stub.dart
@@ -1,4 +1,4 @@
 import 'storage_wrapper.dart';
 
 StorageWrapper getStorageWrapper() => throw UnsupportedError(
-    'Cannot create a storage wrapper without the packages dart:html or package:shared_preferences');
+    'Cannot create a storage wrapper without the packages dart:html or flutter_secure_storage.dart');
