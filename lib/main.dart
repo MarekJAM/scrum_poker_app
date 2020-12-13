2cd7aead400cbc5a1cdca7253b8fcd077648a2db
Marek <marekash@gmail.com>
2020-12-13 17:51:28 +0100

    removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:scrum_poker_app/ui/screens/register_screen.dart';
 import 'package:scrum_poker_app/utils/custom_page_transition_builder.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:http/http.dart' as http;
