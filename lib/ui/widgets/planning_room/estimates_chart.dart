8391e148f5cf43d1b266332277293a09bde74d63
MarekJAM <marekash@gmail.com>
2021-01-27 22:20:01 +0100

    Removed commented out code

--- lib/ui/widgets/planning_room/estimates_chart.dart
+++ lib/ui/widgets/planning_room/estimates_chart.dart
@@ -14,25 +14,6 @@ class EstimatesChart extends StatefulWidget {
 }
 
 class EstimatesChartState extends State {
-  // List<PieChartDataModelUI> list = [
-  //   PieChartDataModelUI(0, 1, Colors.grey),
-  //   PieChartDataModelUI(1, 0, Color(0xFF6cb8dc)),
-  //   PieChartDataModelUI(2, 0, Color(0xFF0a89c4)),
-  //   PieChartDataModelUI(3, 0, Color(0xFF076089)),
-  //   PieChartDataModelUI(4, 0, Color(0xFF054562)),
-  //   PieChartDataModelUI(5, 0, Color(0xFFa0dca9)),
-  //   PieChartDataModelUI(6, 0, Color(0xFF60c46f)),
-  //   PieChartDataModelUI(7, 0, Color(0xFF43895e)),
-  //   PieChartDataModelUI(8, 0, Color(0xFF306238)),
-  //   PieChartDataModelUI(9, 0, Color(0xFFffc766)),
-  //   PieChartDataModelUI(10, 0, Color(0xFFffa200)),
-  //   PieChartDataModelUI(11, 0, Color(0xFFb37100)),
-  //   PieChartDataModelUI(12, 0, Color(0xFF805100)),
-  //   PieChartDataModelUI(13, 0, Color(0xFFe98787)),
-  //   PieChartDataModelUI(14, 0, Color(0xFFe05353)),
-  //   PieChartDataModelUI(15, 0, Color(0xFF9d3a3a)),
-  //   PieChartDataModelUI(20, 0, Color(0xFF702a2a)),
-  // ];
   List<PieChartDataModelUI> list = [];
 
   @override
