e8a9ba257d20ae664b30e9e54873c3bfa4bda2af
MarekJAM <marekash@gmail.com>
2021-03-06 18:09:19 +0100

    Migrated to new snackbar api

--- lib/ui/widgets/common/common_widgets.dart
+++ lib/ui/widgets/common/common_widgets.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CommonWidgets {
-  static void displaySnackBar({@required BuildContext context, @required String message, @required Color color, bool lightText = false}) {
+  static void displaySnackBar({
+    @required BuildContext context,
+    @required String message,
+    @required Color color,
+    bool lightText = false,
+  }) {
     return WidgetsBinding.instance.addPostFrameCallback(
       (_) {
-        Scaffold.of(context).hideCurrentSnackBar();
-        Scaffold.of(context).showSnackBar(
+        ScaffoldMessenger.of(context).hideCurrentSnackBar();
+        ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             backgroundColor: color,
             content: Text(
